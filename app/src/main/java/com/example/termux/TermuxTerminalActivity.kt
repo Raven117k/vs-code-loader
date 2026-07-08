bb96b2fa9fb1b40ab57a1a9f766c3c4b6c3a10e7
Raven117k <luciferplayer31@gmail.com>
2026-07-08 13:39:30 +0500

    Trail Error #36 terminal-view

--- app/src/main/java/com/example/termux/TermuxTerminalActivity.kt
+++ app/src/main/java/com/example/termux/TermuxTerminalActivity.kt
@@ -69,6 +69,7 @@ private fun createSession(environment: TermuxEnvironment): TerminalSession {
         override fun onBell(session: TerminalSession) {}
         override fun onColorsChanged(session: TerminalSession) {}
         override fun onTerminalCursorStateChange(state: Boolean) {}
+        override fun setTerminalShellPid(session: TerminalSession, pid: Int) {}
         override fun getTerminalCursorStyle(): Int? = null
         override fun logError(tag: String?, message: String?) {}
         override fun logWarn(tag: String?, message: String?) {}
@@ -91,20 +92,20 @@ private fun createSession(environment: TermuxEnvironment): TerminalSession {
 
 private class BasicTerminalViewClient : TerminalViewClient {
     override fun onScale(scale: Float): Float = scale
-    override fun onSingleTapUp(e: MotionEvent?): Boolean = false
-    override fun onLongPress(event: MotionEvent?): Boolean = false
+    override fun onSingleTapUp(e: MotionEvent) {}
+    override fun onLongPress(event: MotionEvent): Boolean = false
     override fun shouldBackButtonBeMappedToEscape(): Boolean = false
     override fun shouldEnforceCharBasedInput(): Boolean = true
     override fun shouldUseCtrlSpaceWorkaround(): Boolean = false
     override fun isTerminalViewSelected(): Boolean = true
     override fun copyModeChanged(copyMode: Boolean) {}
-    override fun onKeyDown(keyCode: Int, e: KeyEvent?, session: TerminalSession?): Boolean = false
-    override fun onKeyUp(keyCode: Int, e: KeyEvent?): Boolean = false
+    override fun onKeyDown(keyCode: Int, e: KeyEvent, session: TerminalSession): Boolean = false
+    override fun onKeyUp(keyCode: Int, e: KeyEvent): Boolean = false
     override fun readControlKey(): Boolean = false
     override fun readAltKey(): Boolean = false
     override fun readShiftKey(): Boolean = false
     override fun readFnKey(): Boolean = false
-    override fun onCodePoint(codePoint: Int, ctrlDown: Boolean, session: TerminalSession?): Boolean = false
+    override fun onCodePoint(codePoint: Int, ctrlDown: Boolean, session: TerminalSession): Boolean = false
     override fun onEmulatorSet() {}
     override fun logError(tag: String?, message: String?) {}
     override fun logWarn(tag: String?, message: String?) {}
